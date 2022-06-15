d64e0ba3a62e0e3949d54aeba9d47e10061c3207
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2022-06-16 00:36:45 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const brain = require('brain.js');    //"import brain.js"
+const network = new brain.recurrent.LSTM(); //use brainjs to make a new neural network first step
+const data = require('./data.json')
+network.train([
+
+]);
+
+
