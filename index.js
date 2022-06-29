86ac4ee84ec2c0fad3b5018c98206b5a04f11cec
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2022-06-29 16:05:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const brain = require('brain.js');    //"import brain.js"
 const network = new brain.recurrent.LSTM(); //use brainjs to make a new neural network first step
 const data = require('./data.json')
-network.train([
 
-]);
-
-
+const trainingData = data.map(item => ({
+    input: item.url,
+    output: item.category
+  }));
+  
+  network.train(trainingData, {
+    iterations: 2000
+  });
+  
+  const output = network.run("www.twitter.com");
+  
+  console.log(`Category: ${output}`);
